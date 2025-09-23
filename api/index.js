b992f9a8e744cfcffe9077661a7d494b183957dd
Bruno Nyland <113864915+BrunoNyland@users.noreply.github.com>
2025-09-22 23:24:18 -0400

    Correção na configuração CORS da API

--- api/index.js
+++ api/index.js
@@ -2,22 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const userRoutes = require('./src/controllers/userController');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, '..')));
-
-// Configuração CORS
 app.use(cors({
-  origin: ['*'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: false
 }));
 
 app.use(express.json());
+
 app.use('/api', userRoutes);
+
+app.use(express.static(path.join(__dirname, '..')));
 
 app.use((req, res) => {
   res.status(404).json({ error: 'Rota não encontrada' });
